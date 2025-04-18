392aab60cab633723b47f04a74051a2aead99c17
Jack Darracott <jack@moonfive.co.uk>
2025-04-18 16:33:41 +0100

    wip

--- src/Actions/CheckSite.php
+++ src/Actions/CheckSite.php
@@ -10,34 +10,27 @@ class CheckSite
 
     public function handle(): SiteCheck
     {
-
         return new SiteCheck(
             prev_error_log_count: $this->getPrevErrorLogCount(),
             current_error_log_count: $this->getCurrentErrorLogCount(),
         );
     }
 
-    private function getPrevErrorLogCount(): ?int
+    private function getErrorLogCount(string $date_iso_string): int
     {
-        try {
-            $log = $this->getLog->handle(date("Y-m-d", strtotime("-1 day")));
-            $errors = preg_grep("/production.ERROR/", explode("\n", $log));
+        $log = $this->getLog->handle($date_iso_string);
+        $errors = preg_grep("/production.ERROR/", explode("\n", $log));
 
-            return count($errors);
-        } catch (\Throwable $th) {
-            return null;
-        }
+        return count($errors);
     }
 
-    private function getCurrentErrorLogCount(): ?int
+    private function getPrevErrorLogCount(): int
     {
-        try {
-            $log = $this->getLog->handle(date("Y-m-d"));
-            $errors = preg_grep("/production.ERROR/", explode("\n", $log));
+        return $this->getErrorLogCount(date("Y-m-d", strtotime("-1 day")));
+    }
 
-            return count($errors);
-        } catch (\Throwable $th) {
-            return null;
-        }
+    private function getCurrentErrorLogCount(): int
+    {
+        return $this->getErrorLogCount(date("Y-m-d"));
     }
 }
